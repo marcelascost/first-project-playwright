cf8982fc51123737a506c5ff7735e3c5ed68ba4a
marcelascost <marcelascost@gmail.com>
2026-08-23 17:58:17 -0300

    lógica aplicada aos testes pt 1

--- tests/treinamento/test_conditional_logic_applied_at_tests.py
+++ tests/treinamento/test_conditional_logic_applied_at_tests.py
@@ -3,9 +3,13 @@ from pages.products import Products
 def test_add_product_to_cart_if_less_than_500(page):
     products = Products(page)
     products.access_products()
-    product_price = int(products.card_product.nth(1).locator('productinfo h2').inner_text().replace('Rs. ',''))
+    product_price: int = int(products.card_product.nth(2).locator('.productinfo h2').inner_text().replace('Rs. ',''))
     print(product_price)
     print(type(product_price))
+    if product_price <= 500:
+        products.add_product_to_cart(index=1)
+        print('Produto com valor abaixo de 500 reais')
+    else:
+        print('Produto com valor acima de 500 reais')
     page.pause()
 
-#REVER AULA
